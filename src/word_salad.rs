96d871556fc5db1df0083004d6c8bb7345c5037a
henryk1229 <henryk1229@gmail.com>
2023-11-10 10:58:20 -0500

    feat: wip, improve layering

--- src/word_salad.rs
+++ src/word_salad.rs
@@ -26,28 +26,49 @@ impl WordSalad {
     self.children.insert(word.to_string(), WordSalad::create(word));
   }
 
-  pub fn toss_salad(&mut self, root_word: &str, word_list: &Vec<String>) {
-    // handle root level - TODO - reduce branching?
-    // if self.word.as_ref().unwrap() == root_word {
-    //   for word in word_list.iter() {
-    //       WordSalad::insert_word(self, word)
-    //   }
-    //   // return
-    // };
-    // handle node layers
+  pub fn toss_salad(&mut self, root_word: &str, word_list: &Vec<String>, layer_number: i32) {
+    let root_letters: Vec<char> = root_word.chars().collect();
+    println!("ROOT LETTEERS {:?}", root_letters);
     let current_word = self.word.as_ref().unwrap();
     let child_nodes = self.children.clone();
-    println!("NODESSSSS {:?} ROOT {:?}", child_nodes, root_word);
-    // let used_letters: Vec<char> = root_word.chars().collect();
-    for node in child_nodes {
-      // let used_letters = root_word.to_owned().push(node.0);
-      println!("In here node {:?}", node);
+    // handle root level - TODO - reduce branching?
+    if layer_number == 0 {
       for word in word_list.iter() {
-        println!("In here in here word {}", word);
-        if !root_word.contains(word) && !node.0.contains(word) {
-          WordSalad::insert_word(self, word);
+        // TODO - is there a better way to do this? substring checking?
+        let mut should_insert = true;
+        for letter in root_letters[0..3].iter() {
+          if word.contains(*letter) {
+            should_insert = false;
+          }
+        }
+        if should_insert && word.starts_with(root_letters[4]) {
+          WordSalad::insert_word(self, word)
         }
       }
+    } else {
+      // TODO - debug infinite loop
+      // handle node layers
+      for mut node in child_nodes {
+        // let used_letters = root_word.to_owned().push(node.0);
+        for word in word_list.iter() {
+          println!("In here in here word");
+          let root_letters: Vec<char> = root_word.chars().collect();
+          let mut current_letters: Vec<char> = node.0.to_owned().chars().collect();
+          current_letters.pop();
+          let used_letters: Vec<char> = current_letters.splice(0..current_letters.len() - 1, root_letters).collect();
+          let mut should_insert = true;
+          for letter in used_letters.iter() {
+            if word.contains(*letter) {
+              should_insert = false
+            }
+          }
+          if should_insert {
+            WordSalad::insert_word(&mut node.1, word);
+          }
+        }
+      }
+
+      // println!("ROOT NODE LENGTH {:?}", child_nodes.len());
     }
   }
 }
