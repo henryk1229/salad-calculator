a7134fc9761ae6c604d9bc37d0dfe56079615dbf
henryk1229 <henryk1229@gmail.com>
2023-11-17 10:42:41 -0500

    feat: return string

--- wasm-pkg/src/lib.rs
+++ wasm-pkg/src/lib.rs
@@ -5,13 +5,16 @@ use wasm_bindgen::prelude::*;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
-// #[cfg(feature = "wee_alloc")]
-// #[global_allocator]
-// static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
-pub fn make_word_salad(first_word: String) -> JsValue {
-    let solution_set = salad_calculator::find_word_salads(first_word.as_str());
+pub fn make_word_salad(first_word: String) -> String {
+    let salad_combos: String = salad_calculator::find_word_salads(first_word.as_str())
+        .into_iter()
+        .collect::<Vec<String>>()
+        .join("-");
     
-    serde_wasm_bindgen::to_value(&solution_set).unwrap()
+    salad_combos
 }
